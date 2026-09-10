c0a56da5ccaa94434932f4d50747764b0a7c24cb
JossKM <31990727+JossKM@users.noreply.github.com>
2026-09-10 12:50:36 -0400

    Initial commit

---
+++ game/src/main.cpp
@@ -0,0 +1,35 @@
+/*
+This project uses the Raylib framework to provide us functionality for math, graphics, GUI, input etc.
+See documentation here: https://www.raylib.com/, and examples here: https://www.raylib.com/examples.html
+*/
+
+#include "raylib.h"
+#include "raymath.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+const unsigned int TARGET_FPS = 50;
+float time = 0;
+int main()
+{
+    InitWindow(1200, 800, "Physics-1");
+    SetTargetFPS(TARGET_FPS);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(WHITE);
+            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
+
+
+            time += 1;
+
+            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
